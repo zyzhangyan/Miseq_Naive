51aa23ef1f8e8d0c06219113d0852a20fbc4e5df
yanmc <mcyan90@gmail.com>
2016-08-31 17:36:50 +0800

    IgBLAST done
    from gunzip  to Igblast

--- 1.0.py
+++ 1.0.py
@@ -114,30 +114,101 @@ def unique_fasta(prj_folder):
 		SeqIO.write(seqrecord, writer, "fasta")
 	print "The number of unique reads in fasta file is %d"%len(dict_unique)
 
+def get_assignment_and_recombanation_info(infile):
+	fname = retrieve_name_body(infile)
+	count_v,count_d,count_j = 0,0,0 
+	c_v, c_d, c_j, result ='', '', '', []
+	outfile = open("%s/%s_get_assignment_info.txt"%(prj_tree.igblast_data, fname),"w")
+	writer = csv.writer(outfile,delimiter = "\t")
+	outfile2 = open("%s/%s_get_recombanation_info.txt"%(prj_tree.igblast_data, fname),"w")
+	writer2 = csv.writer(outfile2,delimiter = "\t")
+	reader = csv.reader(open(infile,"rU"),delimiter = "\t")
+	for line in reader:
+		con = str(line)
+		con=con.replace('\'','')
+		con=con[1:-1]
+		hit = re.findall('# Query',con)
+		hit_v=re.match(r'^V.+:.+',con)
+		hit_d=re.match(r'^D.+:.+',con)
+		hit_j=re.match(r'^J.+:.+',con)
+		if hit_v:
+			a_v=hit_v.group()
+			b_v = a_v.split(',')
+			if not(c_v == b_v[1]):
+				c_v=b_v[1]
+				count_v += 1
+				writer.writerow(b_v)
+		if hit_d:
+			a_d=hit_d.group()
+			b_d = a_d.split(',')
+			if not(c_d == b_d[1]):
+				c_d=b_d[1]
+				count_d += 1
+				writer.writerow(b_d)
+		if hit_j:
+			a_j=hit_j.group()
+			b_j = a_j.split(',')
+			if not(c_j == b_j[1]):
+				c_j=b_j[1]
+				count_j += 1
+				writer.writerow(b_j)
+		
+		if hit:
+			con = [':'.join(con.split(":")[1:])]
+			con[0].replace(" ", "")
+			result.append(con)
+		if len(line) >= 7:
+			if line[-1] == '+' or line[-1] == '-':
+				result[-1].extend(line)
+	writer2.writerows(result)
+
 def main():
 	print "Begin!"
+	#'''
+	
+	print "Gunzip..."
+	try:
+		infiles = glob.glob("%s/*.fastq.gz"%(prj_tree.origin))
+		infiles = sorted(infiles)
 
+		print infiles[0]
+		print infiles[1]	
+		os.chdir("%s"%(prj_tree.origin))
+		os.system("rm %s/*.fastq"%(prj_tree.origin))
+		for infile in infiles:
+			gunzip = subprocess.call("gunzip %s "%(infile),shell=True)
+	except:
+		print "The zip file is not exists!"
+		pass
 	#'''
-	print "Merging..."
-	infiles = glob.glob("%s/*.fastq"%(prj_tree.origin))
-	infiles = sorted(infiles)
-	print infiles[0]
-	print infiles[1]	
-	os.chdir("%s"%(prj_tree.origin))
-	merge = subprocess.call("pear -j 4 -f %s -r %s -o %s "%(infiles[0],infiles[1], prj_name),shell=True)
+	
 	#'''
-		
+	if os.path.exists("%s/%s.assembled.fastq"%(prj_tree.origin, prj_name)):
+		pass
+	else:
+		print "Merging..."
+		infiles = glob.glob("%s/*.fastq"%(prj_tree.origin))
+		infiles = sorted(infiles)
+		print infiles[0]
+		print infiles[1]	
+		os.chdir("%s"%(prj_tree.origin))
+		merge = subprocess.call("pear -j 4 -q 20 -f %s -r %s -o %s "%(infiles[0],infiles[1], prj_name),shell=True)
 	#'''
-	print "Quality contorl..."
-	infiles = glob.glob("%s/*.assembled.fastq"%(prj_tree.origin))
-	trim_files, bad_list = [], []
-	for the_file in infiles:
-		trim_fastq_by_quality(the_file,prj_folder,bad_list)
+	
+	#'''
+	if os.path.exists("%s/%s.assembled_trimed.fastq"%(prj_tree.origin, prj_name)):
+		pass
+	else:
+		print "Quality contorl..."
+		infiles = glob.glob("%s/*.assembled.fastq"%(prj_tree.origin))
+		trim_files, bad_list = [], []
+		for the_file in infiles:
+			trim_fastq_by_quality(the_file,prj_folder,bad_list)
 	#sys.exit(0)
 	#'''	
 	'''
 	print "Filter..."
-	infiles = glob.glob("%s/*_trimed.fastq"%(prj_tree.origin))
+	infiles = glob.glob("%s/*assembled_trimed.fastq"%(prj_tree.origin))
 	infiles = sorted(infiles)
 	r1_infile, r1_id_list = SeqIO.index(infiles[0], "fastq"), []
 	r2_infile, r2_id_list = SeqIO.index(infiles[1], "fastq"), []
@@ -158,16 +229,20 @@ def main():
 	'''
 	
 	#'''
-	print "Convert fastq to fasta..."
-	merged_file = "%s/%s.assembled_trimed.fastq"%(prj_tree.origin, prj_name)
-	fname, suffix = os.path.splitext(merged_file)
-	count = SeqIO.convert(merged_file, "fastq","%s.fasta"%fname, "fasta")
-	print count
-	print "There are  %i records have been Converted!" %(count)
+	if os.path.exists("%s/%s.assembled_trimed.fasta"%(prj_tree.origin, prj_name)):
+		pass
+	else:
+		print "Convert fastq to fasta..."
+		merged_file = "%s/%s.assembled_trimed.fastq"%(prj_tree.origin, prj_name)
+		fname, suffix = os.path.splitext(merged_file)
+		count = SeqIO.convert(merged_file, "fastq","%s.fasta"%fname, "fasta")
+		print count
+		print "There are  %i records have been Converted!" %(count)
 	#'''
 	'''
 	print "Unique fasta file..."
 	unique_fasta(prj_folder)
+	
 	'''
 	#Step 2: Split to little files
 	print "Step 2: Split to little files"
@@ -179,32 +254,34 @@ def main():
 		handle.close()
 		print "Wrote %i records to %s" % (count, filename)
 	files_num = i+1
-	#"""
+	
 	#'''
 	#Step 5: Mapping, Multiple processing
 	print "Begin IgBLAST..."
+	cmd = "cp -r /zzh_gpfs02/yanmingchen/HJT-PGM/PGM_UMI_121_20160624/Igblast_database/* %s/"%(prj_tree.igblast_database)
+	gunzip = subprocess.call(cmd,shell=True)
 	prepare_IgBLAST_jobs(prj_name, prj_tree)
 	IgBLAST_jobs = glob.glob("%s/IgBLAST_*.sh" %(prj_tree.jobs))
 	IgBLAST_pool = Pool()
 	
-	'''#Cluster PBS
-	IgBLAST_jobs_ids = IgBLAST_pool.map_async(bsub_IgBLAST_jobs, IgBLAST_jobs).get(120)
+	#'''#Cluster PBS
+	IgBLAST_jobs_ids = IgBLAST_pool.map_async(bsub_jobs, IgBLAST_jobs).get(120)
 	print "IgBLAST_jobs: %s IgBLAST_job has been submited."%len(IgBLAST_jobs_ids)
-	check_IgBLAST_done(IgBLAST_jobs_ids)
+	check_jobs_done(prj_name, prj_tree, "IgBLAST", IgBLAST_jobs_ids)
 	print "Waiting for all subprocesses done..."
 	IgBLAST_pool.close()
 	IgBLAST_pool.join()
 	print 'All subprocesses done.'
-	'''
+	#'''
 	
-	#'''#One machine
+	'''#One machine
 	IgBLAST_jobs_ids = IgBLAST_pool.map_async(processing_jobs, IgBLAST_jobs).get(120)
 	print "IgBLAST_jobs: %s IgBLAST_job has been submited."%len(IgBLAST_jobs_ids)
 	print "Waiting for all subprocesses done..."
 	IgBLAST_pool.close()
 	IgBLAST_pool.join()
 	print 'All subprocesses done.'
-	#'''
+	'''
 	#'''
 	
 	#"""
